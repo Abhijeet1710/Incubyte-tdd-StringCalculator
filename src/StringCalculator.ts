4d5a913b3bde36e5e80abb1640756864d9dff2af
Abhijeet1710 <abhijeetkhamkar30@gmail.com>
2025-04-05 12:21:11 +0530

    Passing : Invalid number in string should return 'Invalid input' message with invalid number/input

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,19 +1,31 @@
 export class StringCalculator {
   constructor() {}
 
-  public add(numbers: string): number {
+  public add(numbers: string): number | string {
     let sum = 0;
     if (numbers === "") {
       return sum;
     }
-
-    sum = numbers
-      .split(",")
-      .reduce(
-        (additionTillNow, currNum) => additionTillNow + parseInt(currNum),
-        0
-      );
+    try {
+      sum = numbers
+        .split(",")
+        .reduce(
+          (additionTillNow, currNum) =>
+            additionTillNow + this.getNumber(currNum),
+          0
+        );
+    } catch (e: unknown) {
+      return (e as Error).message;
+    }
 
     return sum;
+  }
+
+  private getNumber(num: string): number {
+    const parsedNum = Number(num);
+    if (isNaN(parsedNum)) {
+      throw new Error(`Invalid Input : ${num}`);
+    }
+    return parsedNum;
   }
 }
