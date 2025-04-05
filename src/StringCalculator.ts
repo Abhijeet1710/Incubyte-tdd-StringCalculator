80b18179e75cf872fbdf6e650cd4a8373416b939
Abhijeet1710 <abhijeetkhamkar30@gmail.com>
2025-04-05 12:01:15 +0530

    Running return 0 for "" case

---
+++ src/StringCalculator.ts
@@ -0,0 +1,12 @@
+export class StringCalculator {
+  constructor() {}
+
+  public add(numbers: string): number {
+    let sum = 0;
+    if (numbers === "") {
+      return sum;
+    }
+
+    return sum;
+  }
+}
--- test/StringCalculator.test.ts
+++ test/StringCalculator.test.ts
@@ -1,5 +1,13 @@
+import { StringCalculator } from "../src/StringCalculator";
+
 describe("StringCalculator : Addition", () => {
+  let stringCalculator: StringCalculator;
+
+  beforeEach(() => {
+    stringCalculator = new StringCalculator();
+  });
+
   it("should return 0 for an empty string", () => {
-    expect(StringCalculator.add("")).toBe(0);
+    expect(stringCalculator.add("")).toBe(0);
   });
 });
