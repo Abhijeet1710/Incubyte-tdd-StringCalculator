861f5bae4949645d75da82711bea44053231c176
Abhijeet1710 <abhijeetkhamkar30@gmail.com>
2025-04-05 12:02:49 +0530

    test case for valid value

--- test/StringCalculator.test.ts
+++ test/StringCalculator.test.ts
@@ -10,4 +10,8 @@ describe("StringCalculator : Addition", () => {
   it("should return 0 for an empty string", () => {
     expect(stringCalculator.add("")).toBe(0);
   });
+
+  it("should return 5 for 1,4", () => {
+    expect(stringCalculator.add("1,4")).toBe(5);
+  });
 });
